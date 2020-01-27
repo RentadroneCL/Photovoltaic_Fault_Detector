2d3f2f7f3bed351afc05bd4e0d3538fb662e55ea
Daniel Saavedra <dlsaavedra@uc.cl>
2020-01-26 22:26:13 -0300

    DataFlit to XML 2

--- DataFlit2xml.py
+++ DataFlit2xml.py
@@ -27,9 +27,9 @@ def mkdir(filename):
         except OSError as exc: # Guard against race condition
             if exc.errno != errno.EEXIST:
                 raise
-                
 
-  
+
+
 
 
 argparser = argparse.ArgumentParser(
@@ -53,69 +53,76 @@ argparser.add_argument(
     #Examplo 'Train_B/'
 
 
-              
+
 def _main_(args):
-    
+
     input_path   = args.input
     output_path = args.output
     thermal_path = args.input_thermal
-    
+
     mkdir(output_path)
     mkdir(output_path + 'images/')
     mkdir(output_path + 'anns/')
-    
-    Excel = pandas.read_excel(input_path, sheet_name= 'Lista_Archivos_Fotos', header= 1)
-    
+
+    Excel = pandas.read_excel(input_path, sheet_name= 'Lista_Archivos_Fotos', header = 1)
+
     for index_path in  range(len(Excel.Archivo)):
-        
+
         if not pandas.notna(Excel.Archivo[index_path]):
             continue
-        
+
+
         path_Flir = Excel.loc[index_path]['Archivo']
         cod_falla = int(Excel.loc[index_path]['Cód. Falla'])
         sev = Excel.loc[index_path]['Severidad']
-        
-        path_Flir_aux = thermal_path  + '/'.join(path_Flir.split('/')[-2:])
-        
+
+        ## Junta las mismas fotos con distintos label EJ :
+        # DJI_0021B ---> DJI_0021
+        aux = path_Flir.split('/')[-2:]
+        if len(aux[1].split('.')[0]) > 8:
+            aux[1] = aux[1].split('.')[0][:8] + '.' + aux[1].split('.')[1]
+
+        path_Flir_aux = thermal_path  + '/'.join(aux)
+
         if not os.path.isfile(path_Flir_aux):
             print ('No existe la imagen', path_Flir_aux)
             continue
-        
+
         flir = flirimageextractor.FlirImageExtractor()
-        
+
         try:
             flir.process_image(path_Flir_aux)
             I = flirimageextractor.FlirImageExtractor.get_thermal_np(flir)
             w, h = I.shape
-            
+
         except:
             print('No se puede leer la imagen Flir', path_Flir_aux)
             continue
-        
+
         dic_data = flir.get_metadata(path_Flir_aux)
         meas = [s for s in dic_data.keys() if "Meas" in s]
         q_bbox = len(meas)//3 # cada bbox tiene 3 parametros
-        
+
         param_bbox = []
         for num_bbox in range(1, q_bbox + 1):
             # Se guarda los parametros de los boundibox (xmin, ymin, width, height) width = xmax- xmin
             param_bbox.append(list(map(int, dic_data['Meas' + str(num_bbox) + 'Params'].split(' '))))
-        
+
         ##### Save Image and create XML annotations type of fault
-        path_save_img = output_path + 'images/' + '_'.join(path_Flir.split('/')[-2:])
-        path_save_anns =  output_path + 'anns/' + '_'.join(path_Flir.split('/')[-2:])
+        path_save_img = output_path + 'images/' + '_'.join(aux)
+        path_save_anns =  output_path + 'anns/' + '_'.join(aux)
         path_save_anns = path_save_anns[:-4] + '.xml'
-        
+
         if not os.path.isfile(path_save_img):
             plt.imsave(path_save_img , I, cmap = 'gray')
-        
+
         #si el archivo ya existe se agregan mas anotaciones
         if os.path.isfile(path_save_anns):
-            
+
             et = ET.parse(path_save_anns)
             root = et.getroot()
             for box in param_bbox:
-    
+
                 obj = ET.SubElement(root, "object")
                 ET.SubElement(obj, "name").text = str(cod_falla)
                 ET.SubElement(obj, "pose").text = 'Unspecified'
@@ -126,13 +133,13 @@ def _main_(args):
                 ET.SubElement(bx, "ymin").text = str(box[1])
                 ET.SubElement(bx, "xmax").text = str(box[0] + box[2])
                 ET.SubElement(bx, "ymax").text = str(box[1] + box[3])
-        
+
             tree = ET.ElementTree(root)
-            tree.write(path_save_anns)      
-        
+            tree.write(path_save_anns)
+
         ## Si no existe se crea desde cero
         else:
-            
+
             root = ET.Element("annotation")
             ET.SubElement(root, "folder").text = output_path[:-1]
             ET.SubElement(root, "filename").text = '_'.join(path_Flir.split('/')[-2:])
@@ -144,9 +151,9 @@ def _main_(args):
             ET.SubElement(size, "height").text = str(h)
             ET.SubElement(size, "depth").text = str(1)
             ET.SubElement(root, "segmented").text = '0'
-        
+
             for box in param_bbox:
-    
+
                 obj = ET.SubElement(root, "object")
                 ET.SubElement(obj, "name").text = str(cod_falla)
                 ET.SubElement(obj, "pose").text = 'Unspecified'
@@ -157,26 +164,26 @@ def _main_(args):
                 ET.SubElement(bx, "ymin").text = str(box[1])
                 ET.SubElement(bx, "xmax").text = str(box[0] + box[2])
                 ET.SubElement(bx, "ymax").text = str(box[1] + box[3])
-        
+
             tree = ET.ElementTree(root)
-            tree.write(path_save_anns)         
-        
-        
-        
-        
-        
-        
-        
-        
-    
-    
+            tree.write(path_save_anns)
+
+
+
+
+
+
+
+
+
+
     files = []
     # r=root, d=directories, f = files
     for r, d, f in os.walk(input_path):
         for file in f:
             if '.jpg' in file:
                 files.append(os.path.join(r, file))
-    
+
     for f in files:
         flir = flirimageextractor.FlirImageExtractor()
         print(f)
@@ -187,10 +194,10 @@ def _main_(args):
             I = plt.imread(f)
         #flir.save_images()
         #flir.plot()
-        
-        
-        
-        #img = img.astype(np.int8) 
+
+
+
+        #img = img.astype(np.int8)
         W = np.where(np.isnan(I))
         if np.shape(W)[1] > 0:
 
@@ -199,14 +206,14 @@ def _main_(args):
             img = I[:ymax,:]
         else:
             img = I
-        
+
         list_string = f.split('/')
         list_string[-3]+= '_jpg'
         f_aux = '/'.join(list_string)
-        
+
         mkdir(f_aux)
         plt.imsave(f_aux, img, cmap = 'gray')
-        
+
 
 
 if __name__ == '__main__':
